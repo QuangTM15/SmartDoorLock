a865300b6cba86e9c104bd585ca7e0bd5b2d8ef7
Toan Nguyen <nguyentoan062411@gmail.com>
2026-02-26 22:21:26 +0700

    Button servo
    Open and close after 10 second

---
+++ src/button.cpp
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo doorServo;
+const int buttonPin = 2;
+const int servoPin = 9;
+
+void setup() {
+    doorServo.attach(servoPin);
+    pinMode(buttonPin, INPUT_PULLUP);
+    doorServo.write(0); 
+}
+
+void loop() {
+    if (digitalRead(buttonPin) == LOW) {
+        doorServo.write(90);
+        delay(10000); 
+        doorServo.write(0); 
+    }
+}
