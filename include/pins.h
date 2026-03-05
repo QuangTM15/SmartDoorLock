4a5b61069c7b9549629c0cd63d1012343936fe48
QuangTM <quangtmhe200224@gmail.com>
2026-03-05 19:46:33 +0700

    Add file pins config

---
+++ include/pins.h
@@ -0,0 +1,38 @@
+#ifndef PINS_H
+#define PINS_H
+
+/* ===============================
+   SERVO DOOR LOCK
+   =============================== */
+#define SERVO_PIN 9
+
+/* ===============================
+   BUZZER
+   =============================== */
+#define BUZZER_PIN 8
+
+/* ===============================
+   BUTTON (OPEN DOOR FROM INSIDE)
+   =============================== */
+#define BUTTON_PIN 7
+
+/* ===============================
+   FINGERPRINT SENSOR (SoftwareSerial)
+   =============================== */
+#define FINGER_RX 2
+#define FINGER_TX 3
+
+/* ===============================
+   BLUETOOTH HC05 (SoftwareSerial)
+   =============================== */
+#define BT_RX 4
+#define BT_TX 5
+
+/* ===============================
+   LCD I2C
+   =============================== */
+#define LCD_ADDRESS 0x27
+#define LCD_COLS 16
+#define LCD_ROWS 2
+
+#endif
