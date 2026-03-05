4a6e3c5ff48bf17d68abb4ab37361ebe7a050c3c
QuangTM <quangtmhe200224@gmail.com>
2026-03-05 22:04:51 +0700

    buzzer handle

---
+++ include/buzzer.h
@@ -0,0 +1,10 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+void initBuzzer();
+
+void beepSuccess();
+void beepError();
+void beepAlarm();
+
+#endif
---
+++ src/buzzer.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+#include "buzzer.h"
+#include "pins.h"
+#include "config.h"
+
+void initBuzzer()
+{
+    pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void beepSuccess()
+{
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(BUZZER_SHORT_BEEP);
+    digitalWrite(BUZZER_PIN, LOW);
+}
+
+void beepError()
+{
+    for(int i=0;i<2;i++)
+    {
+        digitalWrite(BUZZER_PIN, HIGH);
+        delay(BUZZER_SHORT_BEEP);
+
+        digitalWrite(BUZZER_PIN, LOW);
+        delay(100);
+    }
+}
+
+void beepAlarm()
+{
+    for(int i=0;i<10;i++)
+    {
+        digitalWrite(BUZZER_PIN, HIGH);
+        delay(200);
+
+        digitalWrite(BUZZER_PIN, LOW);
+        delay(200);
+    }
+}
